0ea86f5ab74e58fec4423c008a969ec86987e149
Maciek <zdenekj23@o2.pl>
2017-05-02 10:43:59 +0200

    Corrected task moved event listener

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,11 @@ $(function ()
     paragraphs.each(function (index, element)
     {
         var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-        $(element).append(button)
-        $("button").click(function ()
-       {
-          alert($(this).attr("data-tmp"));
-       });
+        $(element).append(button);
+
+    });
+    $("button").click(function()
+    {
+        alert($(this).attr("data-tmp"));
     });
 });
